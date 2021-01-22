21cfe938aa1b4aefe8e0a5e0387a91463a8b285a
wilsonx <mdianer@qq.com>
2021-01-22 19:22:36 +0800

    support canvas context pass in aguments

--- build/heatmap.js
+++ build/heatmap.js
@@ -4,7 +4,7 @@
  * Copyright 2008-2016 Patrick Wied <heatmapjs@patrick-wied.at> - All rights reserved.
  * Dual licensed under MIT and Beerware license 
  *
- * :: 2016-09-05 01:16
+ * :: 2021-01-22 19:20
  */
 ;(function (name, context, factory) {
 
@@ -245,10 +245,8 @@ var Store = (function StoreClosure() {
 
   return Store;
 })();
-
 var Canvas2dRenderer = (function Canvas2dRendererClosure() {
-
-  var _getColorPalette = function(config) {
+  var _getColorPalette = function (config) {
     var gradientConfig = config.gradient || config.defaultGradient;
     var paletteCanvas = document.createElement('canvas');
     var paletteCtx = paletteCanvas.getContext('2d');
@@ -267,12 +265,12 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
     return paletteCtx.getImageData(0, 0, 256, 1).data;
   };
 
-  var _getPointTemplate = function(radius, blurFactor) {
+  var _getPointTemplate = function (radius, blurFactor) {
     var tplCanvas = document.createElement('canvas');
     var tplCtx = tplCanvas.getContext('2d');
     var x = radius;
     var y = radius;
-    tplCanvas.width = tplCanvas.height = radius*2;
+    tplCanvas.width = tplCanvas.height = radius * 2;
 
     if (blurFactor == 1) {
       tplCtx.beginPath();
@@ -280,19 +278,17 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
       tplCtx.fillStyle = 'rgba(0,0,0,1)';
       tplCtx.fill();
     } else {
-      var gradient = tplCtx.createRadialGradient(x, y, radius*blurFactor, x, y, radius);
+      var gradient = tplCtx.createRadialGradient(x, y, radius * blurFactor, x, y, radius);
       gradient.addColorStop(0, 'rgba(0,0,0,1)');
       gradient.addColorStop(1, 'rgba(0,0,0,0)');
       tplCtx.fillStyle = gradient;
-      tplCtx.fillRect(0, 0, 2*radius, 2*radius);
+      tplCtx.fillRect(0, 0, 2 * radius, 2 * radius);
     }
-
-
 
     return tplCanvas;
   };
 
-  var _prepareData = function(data) {
+  var _prepareData = function (data) {
     var renderData = [];
     var min = data.min;
     var max = data.max;
@@ -302,11 +298,11 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
     var xValues = Object.keys(data);
     var xValuesLen = xValues.length;
 
-    while(xValuesLen--) {
+    while (xValuesLen--) {
       var xValue = xValues[xValuesLen];
       var yValues = Object.keys(data[xValue]);
       var yValuesLen = yValues.length;
-      while(yValuesLen--) {
+      while (yValuesLen--) {
         var yValue = yValues[yValuesLen];
         var value = data[xValue][yValue];
         var radius = radi[xValue][yValue];
@@ -326,45 +322,42 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
     };
   };
 
-
   function Canvas2dRenderer(config) {
-    var container = config.container;
-    var shadowCanvas = this.shadowCanvas = document.createElement('canvas');
-    var canvas = this.canvas = config.canvas || document.createElement('canvas');
-    var renderBoundaries = this._renderBoundaries = [10000, 10000, 0, 0];
-
-    var computed = getComputedStyle(config.container) || {};
-
-    canvas.className = 'heatmap-canvas';
-
-    this._width = canvas.width = shadowCanvas.width = config.width || +(computed.width.replace(/px/,''));
-    this._height = canvas.height = shadowCanvas.height = config.height || +(computed.height.replace(/px/,''));
+    var shadowCanvas = (this.shadowCanvas = document.createElement('canvas'));
+    var canvas = (this.canvas = config.canvas || document.createElement('canvas'));
 
     this.shadowCtx = shadowCanvas.getContext('2d');
-    this.ctx = canvas.getContext('2d');
-
-    // @TODO:
-    // conditional wrapper
-
-    canvas.style.cssText = shadowCanvas.style.cssText = 'position:absolute;left:0;top:0;';
-
-    container.style.position = 'relative';
-    container.appendChild(canvas);
-
     this._palette = _getColorPalette(config);
     this._templates = {};
+    this._renderBoundaries = [10000, 10000, 0, 0];
+
+    if (config.context2d) {
+      this.ctx = config.context2d;
+      shadowCanvas.width = config.width;
+      shadowCanvas.height = config.height;
+    } else {
+      var container = config.container;
+      var computed = getComputedStyle(config.container) || {};
+      this.ctx = canvas.getContext('2d');
+      this._width = canvas.width = shadowCanvas.width = config.width || +computed.width.replace(/px/, '');
+      this._height = canvas.height = shadowCanvas.height = config.height || +computed.height.replace(/px/, '');
+      canvas.className = 'heatmap-canvas';
+      canvas.style.cssText = shadowCanvas.style.cssText = 'position:absolute;left:0;top:0;';
+      container.style.position = 'relative';
+      container.appendChild(canvas);
+    }
 
     this._setStyles(config);
-  };
+  }
 
   Canvas2dRenderer.prototype = {
-    renderPartial: function(data) {
+    renderPartial: function (data) {
       if (data.data.length > 0) {
         this._drawAlpha(data);
         this._colorize();
       }
     },
-    renderAll: function(data) {
+    renderAll: function (data) {
       // reset render boundaries
       this._clear();
       if (data.data.length > 0) {
@@ -372,51 +365,54 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
         this._colorize();
       }
     },
-    _updateGradient: function(config) {
+    _updateGradient: function (config) {
       this._palette = _getColorPalette(config);
     },
-    updateConfig: function(config) {
+    updateConfig: function (config) {
       if (config['gradient']) {
         this._updateGradient(config);
       }
       this._setStyles(config);
     },
-    setDimensions: function(width, height) {
+    setDimensions: function (width, height) {
       this._width = width;
       this._height = height;
       this.canvas.width = this.shadowCanvas.width = width;
       this.canvas.height = this.shadowCanvas.height = height;
     },
-    _clear: function() {
+    _clear: function () {
       this.shadowCtx.clearRect(0, 0, this._width, this._height);
       this.ctx.clearRect(0, 0, this._width, this._height);
     },
-    _setStyles: function(config) {
-      this._blur = (config.blur == 0)?0:(config.blur || config.defaultBlur);
+    _setStyles: function (config) {
+      this._blur = config.blur == 0 ? 0 : config.blur || config.defaultBlur;
 
       if (config.backgroundColor) {
         this.canvas.style.backgroundColor = config.backgroundColor;
       }
 
-      this._width = this.canvas.width = this.shadowCanvas.width = config.width || this._width;
-      this._height = this.canvas.height = this.shadowCanvas.height = config.height || this._height;
-
+      if (config.context2d) {
+        this._width = config.width;
+        this._height = config.height;
+      } else {
+        this._width = this.canvas.width = this.shadowCanvas.width = config.width || this._width;
+        this._height = this.canvas.height = this.shadowCanvas.height = config.height || this._height;
+      }
 
       this._opacity = (config.opacity || 0) * 255;
       this._maxOpacity = (config.maxOpacity || config.defaultMaxOpacity) * 255;
       this._minOpacity = (config.minOpacity || config.defaultMinOpacity) * 255;
       this._useGradientOpacity = !!config.useGradientOpacity;
     },
-    _drawAlpha: function(data) {
-      var min = this._min = data.min;
-      var max = this._max = data.max;
+    _drawAlpha: function (data) {
+      var min = (this._min = data.min);
+      var max = (this._max = data.max);
       var data = data.data || [];
       var dataLen = data.length;
       // on a point basis?
       var blur = 1 - this._blur;
 
-      while(dataLen--) {
-
+      while (dataLen--) {
         var point = data[dataLen];
 
         var x = point.x;
@@ -429,9 +425,6 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
         var rectY = y - radius;
         var shadowCtx = this.shadowCtx;
 
-
-
-
         var tpl;
         if (!this._templates[radius]) {
           this._templates[radius] = tpl = _getPointTemplate(radius, blur);
@@ -440,29 +433,28 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
         }
         // value from minimum / value range
         // => [0, 1]
-        var templateAlpha = (value-min)/(max-min);
+        var templateAlpha = (value - min) / (max - min);
         // this fixes #176: small values are not visible because globalAlpha < .01 cannot be read from imageData
-        shadowCtx.globalAlpha = templateAlpha < .01 ? .01 : templateAlpha;
+        shadowCtx.globalAlpha = templateAlpha < 0.01 ? 0.01 : templateAlpha;
 
         shadowCtx.drawImage(tpl, rectX, rectY);
 
         // update renderBoundaries
         if (rectX < this._renderBoundaries[0]) {
-            this._renderBoundaries[0] = rectX;
-          }
-          if (rectY < this._renderBoundaries[1]) {
-            this._renderBoundaries[1] = rectY;
-          }
-          if (rectX + 2*radius > this._renderBoundaries[2]) {
-            this._renderBoundaries[2] = rectX + 2*radius;
-          }
-          if (rectY + 2*radius > this._renderBoundaries[3]) {
-            this._renderBoundaries[3] = rectY + 2*radius;
-          }
-
+          this._renderBoundaries[0] = rectX;
+        }
+        if (rectY < this._renderBoundaries[1]) {
+          this._renderBoundaries[1] = rectY;
+        }
+        if (rectX + 2 * radius > this._renderBoundaries[2]) {
+          this._renderBoundaries[2] = rectX + 2 * radius;
+        }
+        if (rectY + 2 * radius > this._renderBoundaries[3]) {
+          this._renderBoundaries[3] = rectY + 2 * radius;
+        }
       }
     },
-    _colorize: function() {
+    _colorize: function () {
       var x = this._renderBoundaries[0];
       var y = this._renderBoundaries[1];
       var width = this._renderBoundaries[2] - x;
@@ -492,11 +484,9 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
       var len = imgData.length;
       var palette = this._palette;
 
-
-      for (var i = 3; i < len; i+= 4) {
+      for (var i = 3; i < len; i += 4) {
         var alpha = imgData[i];
         var offset = alpha * 4;
-
 
         if (!offset) {
           continue;
@@ -517,20 +507,18 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
           }
         }
 
-        imgData[i-3] = palette[offset];
-        imgData[i-2] = palette[offset + 1];
-        imgData[i-1] = palette[offset + 2];
+        imgData[i - 3] = palette[offset];
+        imgData[i - 2] = palette[offset + 1];
+        imgData[i - 1] = palette[offset + 2];
         imgData[i] = useGradientOpacity ? palette[offset + 3] : finalAlpha;
-
       }
 
       img.data = imgData;
       this.ctx.putImageData(img, x, y);
 
       this._renderBoundaries = [1000, 1000, 0, 0];
-
     },
-    getValueAt: function(point) {
+    getValueAt: function (point) {
       var value;
       var shadowCtx = this.shadowCtx;
       var img = shadowCtx.getImageData(point.x, point.y, 1, 1);
@@ -538,15 +526,14 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
       var max = this._max;
       var min = this._min;
 
-      value = (Math.abs(max-min) * (data/255)) >> 0;
+      value = (Math.abs(max - min) * (data / 255)) >> 0;
 
       return value;
     },
-    getDataURL: function() {
+    getDataURL: function () {
       return this.canvas.toDataURL();
     }
   };
-
 
   return Canvas2dRenderer;
 })();
